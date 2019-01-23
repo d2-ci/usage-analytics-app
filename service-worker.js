673e61a07432e74fc563e4dc94fdbcfb0186ca04
Hendrik de Graaf <hendrik@dhis2.org>
2019-01-23 14:06:05 +0000

    e46e049 chore(build): use relative public path

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.70943afead11f6d9999901c29f08a71f.js"
+  "./precache-manifest.c665d56d73da4aca70d3cd8491e5d9ce.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
