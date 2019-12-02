19f6b4355e8b07c31298f145805ec22a5ffd309e
GitHub <noreply@github.com>
2019-12-02 08:27:19 +0000

    35662df chore(deps): bump react from 16.11.0 to 16.12.0

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.4d0ad941e87bd88c7ecdc71028763295.js"
+  "./precache-manifest.fc98a416e5d6d440bf799f6469032614.js"
 );
 
 self.addEventListener('message', (event) => {
